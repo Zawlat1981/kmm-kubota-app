d0d57b25bb06062b3669ffc0bdcaff7ab2062f07
Aung Zawlat <103322801+Zawlat1981@users.noreply.github.com>
2026-06-25 09:35:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import datetime
 from openai import OpenAI
 
 # ၁။ Page Config
-st.set_page_config(page_title="KMM Kubota Price List", page_icon="🚜", layout="centered")
+st.set_page_config(page_title="KMM Kubota Price List", page_icon="🚜", layout="centered") 
 
 # Google Sheet ID
 SHEET_ID = "1QqQvPKH7G0hqqhd_0V6cP40Htl8qdFEZ6nHBVe_53_g" 
